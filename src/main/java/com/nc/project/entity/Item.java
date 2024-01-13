90232ef09e73f4e84a538f929e16dd6762eb0573
Selah <qwr7652@gmail.com>
2024-01-13 20:48:26 +0900

    Refactor : 아이템 엔티티 수정 및 아이템 Repository파일 추가

--- src/main/java/com/nc/project/entity/Item.java
+++ src/main/java/com/nc/project/entity/Item.java
@@ -1,44 +1,42 @@
-//package com.nc.project.entity;
-//
-//import com.nc.project.dto.ItemDTO;
-//import com.fasterxml.jackson.annotation.JsonBackReference;
-//import jakarta.persistence.*;
-//import lombok.*;
-//
-//import java.util.List;
-//
-//@Entity
-//@Table(name = "ITEM")
-//@Getter
-//@Setter
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Builder
-//public class Item {
-//
-//    @ManyToOne
-//    @JoinColumn(name = "CART_ITEM_ID")
-//    @JsonBackReference
-//    private CartItem cartItem;
-//
-//    @Id
-//    private int itemId;
-//    private String itemName;
-//    private String itemDescription;
-//    private int itemStock;
-//    private char itemStatus;
-//    private String itemCategory;
-//    private int itemPrice;
-//
-////    public ItemDTO toDTO() {
-////        return ItemDTO.builder()
-////                .itemId(this.itemId)
-////                .itemName(this.itemName)
-////                .itemDescription(this.itemDescription)
-////                .itemStock(this.itemStock)
-////                .itemStatus(this.itemStatus)
-////                .itemCategory(this.itemCategory)
-////                .itemPrice(this.itemPrice)
-////                .build();
-////    }
-//}
+package com.nc.project.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Entity
+@Table(name="item")
+@Getter
+@Setter
+@ToString
+public class Item {
+    @Id
+    @Column (name="item_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name="item_name")
+    private String itemNm;
+
+    @Column(name="item_description")
+    private String itemDetail;
+
+    @Column(name="item_stock")
+    private int stockNumber;
+
+    @Column (name="item_stock")
+    private String itemSellStatus;
+
+    @Column (name="item_category")
+    private String itemCategory;
+
+    @Column (name="item_price")
+    private int price;
+
+    @Column (name="cart_item_id")
+    private Long cartItemId;
+
+
+}
