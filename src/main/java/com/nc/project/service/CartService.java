3131c8bf35142a476a461a928781937d54f61b45
Shinjiwoooo <yuljeonjww@gmail.com>
2024-01-22 19:05:03 +0900

    no message

--- src/main/java/com/nc/project/service/CartService.java
+++ src/main/java/com/nc/project/service/CartService.java
@@ -7,6 +7,8 @@ import com.nc.project.entity.Cart;
 import com.nc.project.entity.CartItem;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 
 public interface CartService {
@@ -18,7 +20,7 @@ public interface CartService {
     public List<CartItem> getCartItem (long id);
 
     // 장바구니의 상품목록의 상품 개수를 버튼으로 바꿔주는 기능 (완료)
-    public CartItem updateCartItemCount (Long cartItemId, String action);
+    public Map<String, Integer> updateCartItemCount (Long cartItemId, String action);
 
     // 장바구니에서 목록의 장바구니 아이템을 X버튼으로 삭제하는 기능
     public void deleteCartItem (long cartItemId);
