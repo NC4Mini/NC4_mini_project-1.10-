a5101f8b601c52d20b0fd5209b3043a4cf9deae1
Mooho Lee <dlangh6632@naver.com>
2024-01-26 10:14:27 +0900

    Fix: 패스워드 인코딩 지점 변경으로 인한 시큐리티 설정변경 및 초기 데이터 변경

--- src/main/java/com/nc/project/configuration/SecurityConfig.java
+++ src/main/java/com/nc/project/configuration/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -14,7 +15,7 @@ public class SecurityConfig {
 
     @Bean
     public static PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
     @Bean
