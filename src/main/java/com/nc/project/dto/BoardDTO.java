edd0f2a1705703f6ae6e02556d522133939a829d
Shinjiwoooo <yuljeonjww@gmail.com>
2024-01-29 15:12:10 +0900

    feat:첨부파일 요청보내기 및 파일 로컬에저장

--- src/main/java/com/nc/project/dto/BoardDTO.java
+++ src/main/java/com/nc/project/dto/BoardDTO.java
@@ -3,6 +3,7 @@ package com.nc.project.dto;
 import com.nc.project.entity.Board;
 import lombok.*;
 import org.springframework.cglib.core.Local;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
 
@@ -21,6 +22,12 @@ public class BoardDTO {
   private int  boardHits;
   private LocalDateTime boardCreatedTime;
   private LocalDateTime boardUpdatedTime;
+
+  private MultipartFile boardFile; //save.html => Controller 파일 담는 용도
+  private String originalFileName;  //원본 파일 이름
+  private String storedFileName; // 서버 저장용 파일이름
+  private int fileAttached; //파일 첨부 여부(첨부1, 미첨부0)
+
   public static  BoardDTO toBoardDTO(Board board){
       BoardDTO boardDTO = new BoardDTO();
       boardDTO.setId(board.getId());
