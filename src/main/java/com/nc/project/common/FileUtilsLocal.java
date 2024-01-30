e304c6398bb8213a666376cd46129f7455a5df56
deonkeon41 <deonkeon41@gmail.com>
2024-01-30 11:11:35 +0900

    feat : FIleUtilsLocal @Component 부여

--- src/main/java/com/nc/project/common/FileUtilsLocal.java
+++ src/main/java/com/nc/project/common/FileUtilsLocal.java
@@ -1,6 +1,8 @@
 package com.nc.project.common;
 
 import com.nc.project.dto.ItemFileDTO;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -10,6 +12,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.UUID;
 
+@Component
 public class FileUtilsLocal {
     public static ItemFileDTO parseFileInfo(MultipartFile multipartFile, String attachPath) {
         // 리턴할 ItemFileDTO 객체 생성
